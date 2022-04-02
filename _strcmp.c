7cba099afde6ec14e1ce71f0e01f1416c95daa25
YoussefJell <youssef.jellouli@yahoo.com>
2022-04-02 22:23:56 +0000

    add

--- _strcmp.c
+++ _strcmp.c
@@ -7,11 +7,10 @@
  */
 int _strcmp(const char *s1, const char *s2)
 {
-	unsigned int i = 0, result;
+	unsigned int i = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
 
 	return (s1[i] - s2[i]);
 }
-
