66bc81564547cfdba8225c856566056dab291cdf
AhmedElzowawi <ahmed.elzowawi@gmail.com>
2022-04-09 23:07:55 +0000

    free first argument => args[0]

--- execute_arg.c
+++ execute_arg.c
@@ -5,34 +5,43 @@
  * @splitPath: path value
  * Return: 1 for loop
  */
-int execute_arg(char **args, char **splitPath)
+int execute_arg(char **args, char **splitPath, char *string)
 {
 	pid_t child;
 	int status;
 
 	if (args[0] == NULL)
 		return (1);
-	if (check_built_in(args))
+	if (check_built_in(args, splitPath, string))
 		return (1);
 	args[0] = check_path(args[0], splitPath);
 	if (args[0] != NULL)
 	{
 		child = fork();
 		if (child < 0)
+		{
+			free(args[0]);
 			perror("Error in forking");
+		}
 
 		if (child == 0)
 		{
 			if (execve(args[0], args, environ) == -1)
 			{
+				free(args[0]);
 				perror("./hsh");
 			}
 			exit(-1);
 		}
 		else
+		{
 			wait(&status);
+		}
 	}
 	else
+	{
 		perror("./hsh");
+		
+	}
 	return (1);
 }
