6fea3f3caf2fac43f3a3b308dda827c9372216c3
AhmedElzowawi <ahmed.elzowawi@gmail.com>
2022-04-12 19:45:43 +0000

    add _puts function

--- strUtils_2.c
+++ strUtils_2.c
@@ -13,4 +13,13 @@ int _isdigit(int c)
 	else
 		return (0);
 }
+/**
+ * _puts - check if c is an integer
+ * @string: argv[1]
+ * Return: error
+ */
+int _puts(const char *string)
+{
+	return (write(1, string, _strlen(string)));
+}
 
