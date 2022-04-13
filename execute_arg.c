98d3221433313025ce196150a63f55e1168a9570
AhmedElzowawi <ahmed.elzowawi@gmail.com>
2022-04-13 09:26:08 +0000

    execute test

--- execute_arg.c
+++ execute_arg.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#define UNUSEDVAR __attribute__((unused))
 /**
  * execute_arg - executes arguments from split
  * @args: arguments to execute
@@ -6,22 +7,21 @@
  * @string: string input from read_line
  * Return: 1 for loop
  */
-int execute_arg(char **args, char **splitPath, char *string)
+int execute_arg(char **args, char **splitPath, char *string UNUSEDVAR)
 {
 	pid_t child;
 	int status;
 
 	if (args[0] == NULL)
 		return (1);
-	if (check_built_in(args, splitPath, string))
-		return (1);
+	if (_strcmp(args[0], "exit") == 0)
+		return (0);
 	args[0] = check_path(args[0], splitPath);
 	if (args[0] != NULL)
 	{
 		child = fork();
 		if (child < 0)
 		{
-			free(args[0]);
 			perror("Error in forking");
 		}
 
@@ -29,7 +29,6 @@ int execute_arg(char **args, char **splitPath, char *string)
 		{
 			if (execve(args[0], args, environ) == -1)
 			{
-				free(args[0]);
 				perror("./hsh");
 			}
 			exit(-1);
