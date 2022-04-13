c9d649485650b6d4eb57cb49e0be9634ee68eca2
AhmedElzowawi <ahmed.elzowawi@gmail.com>
2022-04-13 19:36:01 +0000

    remove cd prototype

--- main.h
+++ main.h
@@ -40,6 +40,5 @@ int _atoi(char *c);
 int _isdigit(int c);
 int check_built_in(char **args, char **splitPath, char *string);
 int _puts(const char *string);
-int change_directory(char **args, char **splitPath, char *string);
 
 #endif
